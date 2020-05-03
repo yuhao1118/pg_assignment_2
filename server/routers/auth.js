eb9d372ced41ed451a9c388fdbea58c0f9e36a76
yuhao1118 <1416964247@qq.com>
2020-05-03 22:17:23 +0800

    ajax

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -15,13 +15,4 @@ auth.get('/login_status', (req, res) => {
     res.json(authEntity.loginStatus(req.session))
 })
 
-auth.get('/getLoggedInUser', (req, res) => {
-    utils.interceptLogin(req, () => {
-        let user = _.find(fake_users.public, (o) => { return o.userId === req.session.userId; })
-        res.json(jsonResult.success('OK', user));
-    }, () => {
-        res.json(jsonResult.authFail())
-    })
-})
-
 module.exports = auth;
